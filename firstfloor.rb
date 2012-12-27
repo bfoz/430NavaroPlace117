bbb9a0723afe570104dcdd52c5ea3d84f311368d
Brandon Fosdick <bfoz@bfoz.net>
2012-12-27 14:44:15 -0800

    Added doors to the first floor

--- firstfloor.rb
+++ firstfloor.rb
@@ -49,18 +49,7 @@ inner_wall = Polygon.build do
     up		43.inch
     move	-26.25.inch*Math.cos(45*Math::PI/180), 26.25.inch*Math.sin(45*Math::PI/180)
     up		94.inch
-    left	5.inch		# Into the understair closet
-    down	94.inch		# Fake wall length
-    move	27.inch*Math.cos(45*Math::PI/180), -27.inch*Math.sin(45*Math::PI/180)
-    down	39.625.inch
-    left	10.25.inch
-    down	26.125.inch
-    left	(4.5+36+8.75).inch
-    up		(94+27.inch*Math.sin(45*Math::PI/180)+39.625+26.125).inch
-    up		(34.25 + 3.25).inch
-    right	40.inch
-    down	3.25.inch
-    right	5.inch		# Leaving the understair closet
+    up		34.25.inch
     up		8.125.inch
     left	45.75.inch
     up		(109.875 + 25 - 80.5).inch
@@ -74,15 +63,35 @@ inner_wall = Polygon.build do
     move_vertical_to 0
 end
 
+# The boundary of the understair closet and the front foyer
+hallway_inner_wall = Polygon.build do
+    start_at    [-14.72.inch, -23.28.inch]
+    up      26.125.inch
+    right   10.25.inch
+    up      39.625.inch
+    move    -27.inch*Math.cos(45*Math::PI/180), 27.inch*Math.sin(45*Math::PI/180)
+    up      (94 + 34.25 + 3.25).inch
+    left    40.inch
+    down    216.342.inch
+end
+
+outer_wall = inner_wall.outset(wall_thickness) + hallway_inner_wall.outset(wall_thickness)
+
 model :FirstFloorWalls do
     extrude wall_height do
-	push inner_wall.outset(wall_thickness)
+	push outer_wall
 	push inner_wall
+	push hallway_inner_wall
     end
 
     # The weird bump in the wall
     extrude 53.inch, :origin => [-64.72.inch, (332.812 - 85.375).inch] do
-    	rectangle [0,0], [6.75.inch, 85.375.inch]
+        rectangle [0,0], [6.75.inch, 85.375.inch]
+    end
+
+    # The cutout for the door to the understairs closet
+    extrude 5.inch, :origin => [-26.25.inch*Math.cos(45*Math::PI/180), (26.25.inch*Math.sin(45*Math::PI/180)+43+94)], :x => Y, :y => Z do
+        rectangle [0,0], [34.5.inch, 80.inch]
     end
 
     # The cutout for the garage door
