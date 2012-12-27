b6b29de002022b4845928313f292ba795da0bfde
Brandon Fosdick <bfoz@bfoz.net>
2012-12-27 14:41:22 -0800

    Added doors and windows to the third floor

--- thirdfloor.rb
+++ thirdfloor.rb
@@ -10,175 +10,76 @@ X = Vector[1,0,0]
 Y = Vector[0,1,0]
 Z = Vector[0,0,1]
 
-sketch :ThirdFloorplanSketch do
-    inner_wall = polygon do
-    	start_at    	[0.inch,0.inch]
-    	move_y	    	15.inch
-    	move_x	    	11.75.inch
-    	move_y		39.inch
-    	move_x		4.875.inch	# Door jamb into Bedroom#2
-    	move_y		-70.5.inch
-    	move_x		140.25.inch
-    	move_y		18.inch
-    	move_x		19.875.inch
-    	move_y		39.375.inch
-    	move_y		58.75.inch	# Width of the window
-    	move_y		11.125.inch
-    	move_x		-13.75.inch
-    	move_y		5.inch		# Door jamb into the closet
-    	move_x		13.75.inch
-    	move_y		23.25.inch
-    	move_x		-103.inch	# Back wall of the closet
-    	move_y		-23.25.inch
-    	move_x		6.625.inch
-    	move_y		-5.inch		# Door jamb out of the closet
-    	move_x		-14.inch
-    	move_y		5.inch		# Door jamb into Bedroom2's bathroom
-    	move_x		2.375.inch
-    	move_y		28.75.inch
-    	move_x		62.25.inch
-    	move_y		17.5.inch
-     	move_y		22.75.inch	# Width of the bathroom window
-     	move_y		18.5.inch
-     	move_x		-96.75.inch
-     	move_y		-87.5.inch
-     	move_x		2.375.inch
-     	move_y		-5.inch		# Door jamb out of Bedroom2's bathroom
-     	move_x		-19.75.inch
-     	move_y		-26.75.inch
-     	move_x		-4.875.inch	# Door jamb out of Bedroom2
-     	move_y		5.25.inch
-     	move_y		21.5.inch	# Depth of shelf
-     	move_x		-53.inch
-     	move_y		-27.75.inch
-     	move_x		-5.inch		# Door jamb into Bedroom1
-    	move_y		32.5.inch
-     	move_x		15.25.inch
-     	move_y		25.5.inch
-     	move_x		5.inch		# Door jamb into Bedroom1's closet
-     	move_y		-25.25.inch
-     	move_x		39.inch
-     	move_y		16.5.inch
-     	move_x		16.5.inch
-     	move_y		71.5.inch
-     	move_x		-55.inch
-     	move_y		-31.inch
-     	move_x		-5.inch		# Door jamb out of Bedroom1's closet
-     	move_y		31.inch
-     	move_x		-121.5.inch
-     	move_y		-(4.75+22.785+6.75+22.875+5.5).inch	# The shower wall with the windows
-     	move_x		68.375.inch
-    	move_y		-8.125.inch
-    	move_x		-4.75.inch	# Door jamb into Bedroom1's bathroom
-    	move_y		3.25.inch
-    	move_x		-63.5.inch
-    	move_y		-35.inch
-    	move_x		63.5.inch
-    	move_y		3.25.inch
-    	move_x		4.75.inch	# Door jamb out of Bedroom1's bathroom
-    	move_y		-8.5.inch
-    	move_x		-106.5.inch
-    	move_y		-156.75.inch
-    	move_x		144.75.inch
-    	move_y		84.5.inch
-    	move_x		5.inch		# Door jamb out of Bedroom1
-	move_y		-23.7.inch
-#    	move_x		41.25.inch	# Edge of the steps
-#    	close
-    end
-
-    push inner_wall.outset(6.inch)	# Create the outer wall by outsetting the inner wall by 6 inches
-
+bedroomA = Polygon.build do
+    start_at	[-45.125.inch, -60.875.inch]
+    up		176.285.inch
+    right	15.25.inch
+    up		88.25.inch
+    left	121.5.inch
+    down	62.66.inch
+    right	68.375.inch
+    down	45.125.inch
+    left	106.5.inch
+    down	156.75.inch
 end
 
-sketch :InnerWallSketch do
-    polygon do
-	start_at    	[11.75.inch, -22.5.inch]
-	move_y		(39+37.5).inch
-	move_x		4.875.inch	# Door jamb into Bedroom#2
-	move_y		-70.5.inch
-	move_x		140.25.inch
-	move_y		18.inch
-	move_x		19.875.inch
-	move_y		39.375.inch
-	move_y		58.75.inch	# Width of the window
-	move_y		11.125.inch
-	move_x		-13.75.inch
-	move_y		5.inch		# Door jamb into the closet
-	move_x		13.75.inch
-	move_y		23.25.inch
-	move_x		-103.inch	# Back wall of the closet
-	move_y		-23.25.inch
-	move_x		6.625.inch
-	move_y		-5.inch		# Door jamb out of the closet
-	move_x		-14.inch
-	move_y		5.inch		# Door jamb into Bedroom2's bathroom
-	move_x		2.375.inch
-	move_y		28.75.inch
-	move_x		62.25.inch
-	move_y		17.5.inch
-	move_y		22.75.inch	# Width of the bathroom window
-	move_y		18.5.inch
-	move_x		-96.75.inch
-	move_y		-87.5.inch
-	move_x		2.375.inch
-	move_y		-5.inch		# Door jamb out of Bedroom2's bathroom
-	move_x		-19.75.inch
-	move_y		-26.75.inch
-	move_x		-4.875.inch	# Door jamb out of Bedroom2
-	move_y		5.25.inch
-	move_y		21.5.inch	# Depth of shelf
-	move_x		-53.inch
-	move_y		-27.75.inch
-	move_x		-5.inch		# Door jamb into Bedroom1
-	move_y		32.5.inch
-	move_x		15.25.inch
-	move_y		25.5.inch
-	move_x		5.inch		# Door jamb into Bedroom1's closet
-	move_y		-25.25.inch
-	move_x		39.inch
-	move_y		16.5.inch
-	move_x		16.5.inch
-	move_y		71.5.inch
-	move_x		-55.inch
-	move_y		-31.inch
-	move_x		-5.inch		# Door jamb out of Bedroom1's closet
-	move_y		31.inch
-	move_x		-121.5.inch
-	move_y		-(4.75+22.785+6.75+22.875+5.5).inch	# The shower wall with the windows
-	move_x		68.375.inch
-	move_y		-8.125.inch
-	move_x		-4.75.inch	# Door jamb into Bedroom1's bathroom
-	move_y		3.25.inch
-	move_x		-63.5.inch
-	move_y		-35.inch
-	move_x		63.5.inch
-	move_y		3.25.inch
-	move_x		4.75.inch	# Door jamb out of Bedroom1's bathroom
-	move_y		-8.5.inch
-	move_x		-106.5.inch
-	move_y		-156.75.inch
-	move_x		144.75.inch
-	move_y		84.5.inch
-	move_x		5.inch		# Door jamb out of Bedroom1
-	move_y		-84.5.inch
-	move_x		216.875.inch
-	move_y		38.285.inch
-    end
+bedroomA_bathroom = Polygon.build do
+    start_at	[-88.125.inch, 101.215.inch]
+    up		35.inch
+    left	63.5.inch
+    down	35.inch
 end
 
-sketch :OuterWallSketch do
-    inner_wall = InnerWallSketch.new.elements.first
-    push inner_wall.outset(6.inch)
-
+bedroomA_closet = Polygon.build do
+    start_at	[-25.75.inch, 115.75.inch]
+    right	39.inch
+    up		16.5.inch
+    right	16.5.inch
+    up		71.5.inch
+    left	55.5.inch
 end
+
+bedroomB = Polygon.build do
+    start_at	[16.625.inch, -16.5.inch]
+    right	140.25.inch
+    up		18.inch
+    right	19.875.inch
+    up		109.25.inch
+    left	160.125.inch
+end
+
+bedroomB_bathroom = Polygon.build do
+    start_at	[34.25.inch, 115.75.inch]
+    right	34.5.inch
+    up		28.75.inch
+    right	62.25.inch
+    up		58.75.inch
+    left	96.75.inch
+end
+
+bedroomB_closet = Polygon.build do
+    start_at	[73.75.inch, 115.75.inch]
+    right	103.inch
+    up		23.25.inch
+    left	103.inch
+end
+
+hallway = Polygon.build do
+    start_at	[-40.125.inch, -60.785.inch]
+    right	216.875.inch
+    up		38.285.inch
+    left	165.inch
+    up		133.25.inch
+    left	51.875.inch
+end
+
+rooms = [bedroomA, bedroomA_bathroom, bedroomA_closet, hallway, bedroomB, bedroomB_bathroom, bedroomB_closet]
+outset_rooms = rooms.map {|p| p.outset WALL_THICKNESS }
 
 model :ThirdFloor do
     extrude WALL_HEIGHT do
-	inner_wall = InnerWallSketch.new.elements.first
-	outer_wall = inner_wall.outset(WALL_THICKNESS)
-	push outer_wall
-	push inner_wall
+        push outset_rooms.reduce(:+)
+        rooms.each {|r| push r}
     end
 
     # The ledge at the top of the stairs
@@ -191,13 +92,15 @@ model :ThirdFloor do
 	end
     end
 
+    door_height = 80.inch
+
     # Master bedroom modifications
     group do
     	master_bathroom_window_width = 22.875.inch
     	master_bathroom_window_height = 23.inch
 
 	# The drop ceiling in the master bathroom
-	extrude 8.75.inch, :origin => [0,0, WALL_HEIGHT-8.75.inch] do
+	xextrude 8.75.inch, :origin => [0,0, WALL_HEIGHT-8.75.inch] do
 	    polygon do
 		start_at	[-83.375, 95.965.inch]
 		move_x	-68.375.inch
@@ -209,16 +112,31 @@ model :ThirdFloor do
 	    end
 	end
 
+        # Cutout for the door into the bedroom
+        extrude 5.inch, :origin => [-40.125.inch, 23.175.inch], :x => Y, :y => Z do
+            rectangle [0,0], [59.291.inch, 81.inch]
+        end
+
+        # Cutout for the door into the WC
+        extrude 5.125.inch, :origin => [-83.inch, 104.465.inch], :x => Y, :y => Z do
+            rectangle [0,0], [28.5.inch, door_height]
+        end
+
+        # Cutout for the closet door
+        extrude 4.125.inch, :origin => [-25.75.inch, 141.inch], :x => Y, :y => Z do
+            rectangle [0,0], [31.75.inch, door_height]
+        end
+
 	# Master bedroom large window
-	extrude WALL_THICKNESS, :origin => [-189.875.inch, (95.965-7.75).inch, 38.25.inch], :x => -Y, :y => Z do
+	extrude WALL_THICKNESS, :origin => [-189.5.inch, (29.215).inch, 38.25.inch], :x => Y, :y => Z do
 	    rectangle [0,0], [59.inch, 58.25.inch]
 	end
 
 	# Shower windows
-	extrude WALL_THICKNESS, :origin => [-151.75.inch, (141+5.5+master_bathroom_window_width).inch, (WALL_HEIGHT-4.inch-master_bathroom_window_height-8.75.inch)], :x => -Y, :y => Z do
+	extrude WALL_THICKNESS, :origin => [-151.375.inch, (141+5.5).inch, (WALL_HEIGHT-4.inch-master_bathroom_window_height-8.75.inch)], :x => Y, :y => Z do
 	    rectangle [0,0], [master_bathroom_window_width, master_bathroom_window_height]
 	end
-	extrude WALL_THICKNESS, :origin => [-151.75.inch, (141+5.5+2*master_bathroom_window_width+6.75).inch, (WALL_HEIGHT-4.inch-master_bathroom_window_height-8.75.inch)], :x => -Y, :y => Z do
+	extrude WALL_THICKNESS, :origin => [-151.375.inch, (141+5.5+master_bathroom_window_width+6.75).inch, (WALL_HEIGHT-4.inch-master_bathroom_window_height-8.75.inch)], :x => Y, :y => Z do
 	    rectangle [0,0], [master_bathroom_window_width, master_bathroom_window_height]
 	end
     end
@@ -226,6 +144,22 @@ model :ThirdFloor do
     # Other master bedroom modifications
     group do
     	bathroom_window_height = 23.inch
+        wall_thickness = 5.inch
+
+        # Cutout for the door into the bedroom
+        extrude 4.875.inch, :origin => [11.75.inch, 54.inch], :x => Y, :y => Z do
+            rectangle [0,0], [30.inch, door_height]
+        end
+
+    	# Cutout for the door into the bathroom
+    	extrude wall_thickness, :origin => [36.625.inch, 110.75.inch], :x => X, :y => Z do
+    	    rectangle [0,0], [29.75.inch, door_height]
+    	end
+
+    	# Cutout for the closet door
+    	extrude wall_thickness, :origin => [80.375.inch, 110.75.inch], :x => X, :y => Z do
+    	    rectangle [0,0], [82.625.inch, door_height]
+    	end
 
     	# Large window
     	extrude WALL_THICKNESS, :origin => [176.75.inch, (1.5+39.375).inch, 38.25.inch], :x => Y, :y => Z do
